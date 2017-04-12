c3b7040eec4505ae6014c33ac4735a4c330464be
Mike Branstein <mike.branstein@gmail.com>
2017-04-11 20:43:12 -0400

    add data binding to example

--- app/main-page.js
+++ app/main-page.js
@@ -1,4 +1,16 @@
+var observableModule = require("data/observable");
 var httpModule = require("http");
+
+var viewModel;
+
+exports.onLoaded = function (args) {
+    var page = args.object;
+    viewModel = new observableModule.Observable({
+        httpResponse: ""
+    });
+    
+    page.bindingContext = viewModel;
+};
 
 exports.onGetTap = function (args) {
     console.log("GET button tapped");
@@ -6,7 +18,10 @@ exports.onGetTap = function (args) {
     httpModule
         .request({ url: "https://httpbin.org/get", method: "GET" })
         .then(function (response) {
+            // the HTTP response is converted to a string 
+            // and set to the httpResponse property of the viewModel
             var str = response.content.toString();
+            viewModel.set("httpResponse", str);
             console.log("HTTP Response: " + str);
         }, function (e) {
             // error
